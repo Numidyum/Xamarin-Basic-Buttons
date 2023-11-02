70b04aad88ecdf2398f5922bc8a33e562452be62
Numidyum <aliersoyae2003@gmail.com>
2023-11-02 11:34:46 +0300

    Initial commit

---
+++ Xama/Xama/BasicButtonClickPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xama
+{
+    public partial class BasicButtonClickPage : ContentPage
+    {
+        public BasicButtonClickPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await label.RelRotateTo(360, 1000);
+        }
+    }
+}
---
+++ Xama/Xama/ToggleButtonDemoPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Xama
+{
+	public partial class ToggleButtonDemoPage : ContentPage
+	{
+		public ToggleButtonDemoPage ()
+		{
+			InitializeComponent ();
+		}
+
+        void OnItalicButtonToggled(object sender, ToggledEventArgs args)
+        {
+            if (args.Value)
+            {
+                label.FontAttributes |= FontAttributes.Italic;
+            }
+            else
+            {
+                label.FontAttributes &= ~FontAttributes.Italic;
+            }
+        }
+
+        void OnBoldButtonToggled(object sender, ToggledEventArgs args)
+        {
+            if (args.Value)
+            {
+                label.FontAttributes |= FontAttributes.Bold;
+            }
+            else
+            {
+                label.FontAttributes &= ~FontAttributes.Bold;
+            }
+        }
+    }
+}
